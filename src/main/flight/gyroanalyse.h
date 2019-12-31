62308b7e6d93d426adaa360886f519eadd9dbe6a
ctzsnooze <chris.thompson@sydney.edu.au>
2019-12-31 12:28:38 +1100

    Simplify and improve Dynamic Notch
    Remove prevCenterFreq[axis]
    Simpler peak detection:
    - cubes, not squares
    - only look one bin either side
    More accurate frequency determination
    Updated annotations and explanations
    Whitespace fixes
    fftStartBin can't be less than 1
    Dynamic PT1 smoothing
    biquad filter on centerFreq removed
    centerFreq's now floats
    
    Limits and better min max logic
    
    constrain within nyquist limits
    
    If gyro rate was 2khz and dynamic notch range max, this will keep the notch max to 960hz.
    
    Simple fixes
    
    Get lowest min above and below the peak
    
    First go at setting DN range by a maxHz

--- src/main/flight/gyroanalyse.h
+++ src/main/flight/gyroanalyse.h
@@ -24,9 +24,7 @@
 
 #include "common/filter.h"
 
-
-// max for F3 targets
-#define FFT_WINDOW_SIZE 32
+#define FFT_WINDOW_SIZE 32 // Max for F3
 
 typedef struct gyroAnalyseState_s {
     // accumulator for oversampled data => no aliasing and less noise
@@ -48,9 +46,8 @@ typedef struct gyroAnalyseState_s {
     float fftData[FFT_WINDOW_SIZE];
     float rfftData[FFT_WINDOW_SIZE];
 
-    biquadFilter_t detectedFrequencyFilter[XYZ_AXIS_COUNT];
-    uint16_t centerFreq[XYZ_AXIS_COUNT];
-    uint16_t prevCenterFreq[XYZ_AXIS_COUNT];
+    float centerFreq[XYZ_AXIS_COUNT];
+
 } gyroAnalyseState_t;
 
 STATIC_ASSERT(FFT_WINDOW_SIZE <= (uint8_t) -1, window_size_greater_than_underlying_type);
